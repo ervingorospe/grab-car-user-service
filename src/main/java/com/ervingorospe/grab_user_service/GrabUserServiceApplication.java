be0d5a14dc7ac0b45512eeaab0ff5cc64c55b912
Ervin Gorospe <ervingorospe.code@gmail.com>
2025-03-09 01:33:59 +0800

    update environment yml files

--- src/main/java/com/ervingorospe/grab_user_service/GrabUserServiceApplication.java
+++ src/main/java/com/ervingorospe/grab_user_service/GrabUserServiceApplication.java
@@ -1,5 +1,6 @@
 package com.ervingorospe.grab_user_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GrabUserServiceApplication {
 
 	public static void main(String[] args) {
+		// Load .env file
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		System.setProperty("JWT_expiration", dotenv.get("JWT_expiration"));
+		System.setProperty("JWT_REFRESH_EXPIRATION", dotenv.get("JWT_REFRESH_EXPIRATION"));
+		System.setProperty("PORT", dotenv.get("PORT_USER_SERVICE"));
+
 		SpringApplication.run(GrabUserServiceApplication.class, args);
 	}
 
