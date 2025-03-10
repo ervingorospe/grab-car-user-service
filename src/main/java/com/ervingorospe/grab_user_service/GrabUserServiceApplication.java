0e744a2574fb1845923e5492f26f329f7677c0a2
Ervin Gorospe <ervingorospe.code@gmail.com>
2025-03-10 17:05:09 +0800

    made urls dynamic and added it in environment

--- src/main/java/com/ervingorospe/grab_user_service/GrabUserServiceApplication.java
+++ src/main/java/com/ervingorospe/grab_user_service/GrabUserServiceApplication.java
@@ -9,16 +9,27 @@ public class GrabUserServiceApplication {
 
 	public static void main(String[] args) {
 		// Load .env file
-		Dotenv dotenv = Dotenv.configure().load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
-		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-		System.setProperty("JWT_expiration", dotenv.get("JWT_expiration"));
-		System.setProperty("JWT_REFRESH_EXPIRATION", dotenv.get("JWT_REFRESH_EXPIRATION"));
-		System.setProperty("PORT", dotenv.get("PORT_USER_SERVICE"));
 
+		String isKubernetes = System.getenv("KUBERNETES_SERVICE_HOST");
+
+		if (isKubernetes == null) {
+			// Running locally (IntelliJ)
+			Dotenv dotenv = Dotenv.configure().load();
+			System.setProperty("DB_URL", dotenv.get("DB_URL"));
+			System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+			System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+			System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
+			System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+			System.setProperty("PORT", dotenv.get("PORT_USER_SERVICE"));
+		} else {
+			// Running in Kubernetes (use environment variables)
+			System.setProperty("DB_URL", System.getenv("DB_URL"));
+			System.setProperty("DB_USERNAME", System.getenv("DB_USERNAME"));
+			System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
+			System.setProperty("DB_DRIVER", System.getenv("DB_DRIVER"));
+			System.setProperty("JWT_SECRET", System.getenv("JWT_SECRET"));
+			System.setProperty("PORT", System.getenv("PORT_API_GATEWAY"));
+		}
 		SpringApplication.run(GrabUserServiceApplication.class, args);
 	}
 
